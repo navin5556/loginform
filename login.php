646dc90d51f9bf52a309fa81ea58ee954c07454c
navin5556 <47461555+navin5556@users.noreply.github.com>
2019-02-09 01:33:43 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,14 @@
+<?php
+
+$user = $_REQUEST["username"];
+$pass = $_REQUEST["password"];
+
+if(isset($_REQUEST["submit"])) {
+    if($user=="me" && $pass=="re") {
+       header("Location: welcome.html");
+    } else {
+        echo "Incorrect Login";
+    }
+}
+
+?>
